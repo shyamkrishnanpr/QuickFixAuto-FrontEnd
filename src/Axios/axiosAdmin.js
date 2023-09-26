e325b4332216c861494910063228dbbdc2c32b43
shyam krishnan <shyamkrishnasby@gmail.com>
2023-09-26 13:05:07 +0530

    url updated

--- src/Axios/axiosAdmin.js
+++ src/Axios/axiosAdmin.js
@@ -2,7 +2,8 @@ import axios from "axios";
 import store from "../store/store";
 
 const instance = axios.create({
-  baseURL: "http://localhost:3000",
+  // baseURL: "http://localhost:3000",
+  baseURL:'https://quickfixautos.shop'
 });
 
 instance.interceptors.request.use(
